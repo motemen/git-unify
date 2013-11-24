8dd43e2e74962b8a46fec4d3183526874db0feb8
motemen <motemen@gmail.com>
2013-11-24 23:22:56 +0900

    TODO tests

--- t/test.sh
+++ t/test.sh
@@ -13,26 +13,31 @@ export GIT_UNIFIED_ROOT="$SHARNESS_TRASH_DIRECTORY/.shared-git"
 rm -rf   "$GIT_UNIFIED_ROOT"
 mkdir -p "$GIT_UNIFIED_ROOT"
 
-counter=1
+counter_file="$SHARNESS_TRASH_DIRECTORY/counter"
+echo 1 > "$counter_file"
 
 __git_commit_random () {
+    counter=$(cat "$counter_file")
     echo file $counter >> "FILE_$counter"
     git add FILE_$counter
-    git commit -q -m "commit #$counter"
-    counter=$(( $counter + 1 ))
+    git commit -m "commit #$counter"
+    echo $(( $counter + 1 )) > "$counter_file"
 }
+
+git config --global user.name  'tester'
+git config --global user.email 'test@example.com'
 
 for repo in project-foo project-bar module-a module-b; do
     mkdir -p orig/$repo
 
     ( cd orig/$repo
       git init -q
-      __git_commit_random )
+      __git_commit_random ) >&3 2>&4
 done
 
 ( cd orig/project-foo
   git submodule add -q ../module-a
-  git commit -q -m 'added module-a' )
+  git commit -q -m 'added module-a' ) >&3 2>&4
 
 # done initializing
 
@@ -72,24 +77,25 @@ test_expect_success 'git unify init' '
       git unify init )
 '
 
-test_expect_success 'git unify init - updating branch' '
-    ( cd repo/project-bar   && git checkout -b branch-2 && __git_commit_random && git push origin branch-2 ) &&
-    ( git clone orig/project-bar repo/project-bar-3 ) &&
-    ( cd repo/project-bar   && __git_commit_random && git push origin branch-2 && git checkout master ) &&
-    ( cd repo/project-bar-3 && git checkout    branch-2 && __git_commit_random && git checkout master && git unify init )
-'
-
-test_expect_success 'git unify init - updating branch - branches synced' '
-    ( cd repo/project-bar-3 && git rev-parse branch-2 ) >expected &&
-    ( cd repo/project-bar   && git rev-parse branch-2 ) >actual &&
-    test_cmp expected actual
-'
-
-test_expect_success 'git unify init - updating branch - branches synced' '
-    ( cd repo/project-bar-3 && git rev-parse branch-2 ) >expected &&
-    ( cd repo/project-bar   && git rev-parse branch-2 ) >actual &&
-    test_cmp expected actual
-'
+### TODO
+# test_expect_success 'git unify init - updating branch' '
+#     ( cd repo/project-bar   && git checkout -b branch-2 && __git_commit_random && git push origin branch-2 ) &&
+#     ( git clone orig/project-bar repo/project-bar-3 ) &&
+#     ( cd repo/project-bar   && __git_commit_random && git push origin branch-2 && git checkout master ) &&
+#     ( cd repo/project-bar-3 && git checkout    branch-2 && __git_commit_random && git checkout master && git unify init )
+# '
+# 
+# test_expect_success 'git unify init - updating branch - branches synced' '
+#     ( cd repo/project-bar-3 && git rev-parse branch-2 ) >expected &&
+#     ( cd repo/project-bar   && git rev-parse branch-2 ) >actual &&
+#     test_cmp expected actual
+# '
+# 
+# test_expect_success 'git unify init - updating branch - branches synced' '
+#     ( cd repo/project-bar-3 && git rev-parse branch-2 ) >expected &&
+#     ( cd repo/project-bar   && git rev-parse branch-2 ) >actual &&
+#     test_cmp expected actual
+# '
 
 test_expect_success 'git unify init again fails' '
     ( cd repo/project-bar-2 &&
@@ -116,7 +122,23 @@ test_expect_success 'git unify submodule-add' '
       git unify submodule-add ../../orig/module-b)
 '
 
-# enabling `t/setup.sh`
+test_expect_success 'conflicting branches' '
+    ( cd repo/project-foo-2 &&
+      git checkout -b branch-conflicting &&
+      git push origin branch-conflicting ) &&
+    ( git clone orig/project-foo repo/project-foo-3 &&
+      cd repo/project-foo-3 &&
+      git checkout branch-conflicting &&
+      __git_commit_random ) &&
+    ( cd repo/project-foo-2 &&
+      git checkout branch-conflicting &&
+      __git_commit_random &&
+      git push origin branch-conflicting ) &&
+    ( cd repo/project-foo-3 &&
+      test_must_fail git unify init )
+'
+
+# enabling `./t/setup.sh`
 export SHARNESS_TEST_FILE=${SHARNESS_TEST_FILE#*/}
 
 test_done
